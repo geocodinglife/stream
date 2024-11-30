a41445cecaffbb8458c7e94969080fa9e1eb456e
geocodinglife <geocodinglife@gmail.com>
2024-11-29 22:36:27 -0500

    PWS working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+if("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js").then((registration) => {
+    console.log("Service worker registered with scope:", registration.scope)
+  }).catch((error) => {
+      console.log("Service worker registration failed:", error);
+  })
+}
--- app/views/pwa/service-worker.js
+++ app/views/pwa/service-worker.js
@@ -1,3 +1,29 @@
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = "offline";
+const OFFLINE_URL = "offline.html";
+
+  self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.add(new Request(OFFLINE_URL, {cache: "reload"}))
+      })
+    )
+
+    self.skipWaiting();
+  })
+
+  self.addEventListener("activate", (event) => {
+    event.waitUntil(
+      Promise.resolve().then(() => {
+        if (self.registration.navigationPreload) {
+          return self.registration.navigationPreload.enable()
+        }
+      })
+    )
+
+    self.clients.claim()
+  })
+
 // Add a service worker for processing Web Push notifications:
 //
 // self.addEventListener("push", async (event) => {
